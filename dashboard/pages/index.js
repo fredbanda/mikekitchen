92a2559355507d5ab08c833e91d21917e7b4c0e0
fredbanda <fredcbanda@hotmail.com>
2024-07-13 20:56:54 +0200

    edited

--- dashboard/pages/index.js
+++ dashboard/pages/index.js
@@ -263,11 +263,11 @@ export default function Home() {
       >
         <div className="max-w-xl lg:max-w-3xl">
           <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-            Welcome to Mike&apos;s Dashboard
+            Welcome to Mike&apos;s Kitchen Cabinet Dashboard
           </h1>
 
           <p className="mt-4 leading-relaxed text-gray-500 max-w-sm">
-            From here you will be to add or delete products from your website. This site is only accessible to admin only. Do not share your login details!
+            From here you will be able to add or delete products from your website. This site is only accessible to admin only. Do not share your login details! Please Login by clicking the link below.
           </p>
           <div className="col-span-6 sm:flex sm:items-center sm:gap-4 my-4 flex items-center justify-center">
             <button
